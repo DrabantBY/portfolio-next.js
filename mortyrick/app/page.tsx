d520f952caceabebea9d99ecc81abf6634309455
DrabantBY <drabant2022@gmail.com>
2024-03-10 22:07:15 +0100

    start next project

---
+++ mortyrick/app/page.tsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderComponent />
+      <main>
+        <h1>Hello NEXT</h1>
+      </main>
+    </>
+  );
+}
---
+++ mortyrick/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@mantine/core";
+
+export default function HeaderComponent() {
+  return (
+    <header>
+      <Container>
+        <p>header</p>
+      </Container>
+    </header>
+  );
+}
